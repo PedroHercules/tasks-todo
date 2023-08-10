23831a3cf4575e7a9dbbf97cfc271d7e3927292f
PedroHercules <pedroherculesdantas@gmail.com>
2023-08-10 19:15:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+
+import "./global.css"
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>todo</h1>
+    </header>
+  )
+}
