1597b438f2a4997862354ef92deb8ce2480cfa16
PedroHercules <pedroherculesdantas@gmail.com>
2023-08-19 20:21:28 -0300

    feat: implemnted no tasks container

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Header } from "./components/Header"
 import { Input } from "./components/Input"
 import { PlusCircle } from '@phosphor-icons/react'
 
+import NoteIcon from './assets/no-tasks.svg'
+
 import styles from './app.module.css'
 
 import "./global.css"
@@ -11,14 +13,42 @@ function App() {
   return (
     <>
       <Header />
-      <div className={styles.createTaskContainer}>
-        <Input placeholder="Adicione uma nova tarefa"/>
-        <button className={styles.createTaskBtn}>
-          Criar
-          <PlusCircle size={19} weight="bold"/>
-        </button>
-      </div>
-      <h1>Hello world</h1>
+      <main>
+        <div className={styles.createTaskContainer}>
+          <Input placeholder="Adicione uma nova tarefa"/>
+          <button className={styles.createTaskBtn}>
+            Criar
+            <PlusCircle size={19} weight="bold"/>
+          </button>
+        </div>
+
+        <div className={styles.container}>
+          <div className={styles.taskInfo}>
+            <p>
+              tarefas criadas 
+              <span>0</span>
+            </p>
+            <p>
+              Concluídas
+              <span>0</span>
+            </p>
+          </div>
+
+          <div className={styles.tasksContainer}>
+
+            <div className={styles.noTasks}>
+              <img src={NoteIcon} alt="no tasks" />
+              <p>
+                Você ainda não tem tarefas cadastradas
+                <br />
+                <span>
+                Crie tarefas e organize seus itens a fazer
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
