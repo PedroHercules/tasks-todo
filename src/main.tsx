adc30f62a7feff41afe61367e0c5cf8774520d2c
PedroHercules <pedroherculesdantas@gmail.com>
2023-08-20 20:33:33 -0300

    removed radiux theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Theme } from '@radix-ui/themes';
 
 import App from './App.tsx'
 
@@ -8,8 +7,6 @@ import '@radix-ui/themes/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    <App />
   </React.StrictMode>,
 )
